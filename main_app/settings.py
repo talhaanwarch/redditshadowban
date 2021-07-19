30c00911b5db0b144295dd3c4a8c74d11c9abdbe
Talha Anwar <chtalhaanwar@gmail.com>
2021-07-20 02:31:04 +0500

    migrate db

--- main_app/settings.py
+++ main_app/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ['SECRET_KEY']
 # SECURITY WARNING: keep the secret key used in production secret!
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
