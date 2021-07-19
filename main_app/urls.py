4ac04831d330fdb6e53d2cf2a279d6df8cdaf821
Talha Anwar <chtalhaanwar@gmail.com>
2021-07-20 02:51:06 +0500

    Update urls.py

--- main_app/urls.py
+++ main_app/urls.py
@@ -17,6 +17,8 @@ from django.contrib import admin
 from django.urls import path,include
 from django.urls import path,include,re_path
 from django.conf import settings
+from django.views.static import serve
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('sub_app.urls')),
